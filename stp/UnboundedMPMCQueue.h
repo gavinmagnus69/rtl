673fdd9673c73cc57995e5b7118e220bd55053d4
Roman Akhmetov <r.ahmetov@a.riftek.com>
2025-11-03 11:16:03 +0300

    added new methods to UnboundedMPMCQueue

--- stp/UnboundedMPMCQueue.h
+++ stp/UnboundedMPMCQueue.h
@@ -2,32 +2,42 @@
 #define threadpool_src_unboundedmpmcqueue_h
 
 
+#include <condition_variable>
 #include <deque>
 #include <mutex>
-#include <condition_variable>
 
-namespace rtl{
+namespace rtl {
 namespace stp {
-template<typename T>
+
+// Thread-safe unounded Multi-producer Multi-consumer queue.
+template <typename T>
 class UnboundedMPMCQueue {
 public:
+    // Thread-safe puts and pushes.
     void put(T task) {
         std::lock_guard lock(m_mutex);
         m_buffer.emplace_back(std::move(task));
         m_notEmptyQueue.notify_one();
     }
-
-    //takes and pops
+    // Thread-safe takes and pops. Blocks if empty.
     T take() {
         std::unique_lock lock(m_mutex);
-        while(m_buffer.empty()) {
-            m_notEmptyQueue.wait(lock); //unlocks
+        while (m_buffer.empty()) {
+            m_notEmptyQueue.wait(lock); // unlocks
         }
         return std::move(takeLocked());
     }
     bool empty() {
         std::lock_guard lock(m_mutex);
         return m_buffer.empty();
+    }
+    size_t size() {
+        std::lock_guard lock(m_mutex);
+        return m_buffer.size();
+    }
+    void clear() {
+        std::lock_guard lock(m_mutex);
+        m_buffer.clear();
     }
 private:
     T takeLocked() {
@@ -40,8 +50,8 @@ private:
     std::condition_variable m_notEmptyQueue;
     std::deque<T> m_buffer;
 };
-}
+} // namespace stp
 
 
-};
+}; // namespace rtl
 #endif
