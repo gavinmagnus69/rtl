a43c00bfdeba052db0865c5dbc10dcf5b4954f66
Roman Akhmetov <r.ahmetov@a.riftek.com>
2026-04-22 16:34:25 +0300

    robustness added to RtlThreadPool

--- stp/IExecutor.hpp
+++ stp/IExecutor.hpp
@@ -3,39 +3,49 @@
 #include <functional>
 #include <future>
 
-
 namespace rtl {
 namespace stp {
 
 struct TaskOptions {
-    bool is_periodic{false};
-    size_t periodic_interval_ms{0};
+  bool is_periodic{false};
+  size_t periodic_interval_ms{0};
 };
 
 using Task = std::function<void()>;
 
 struct IExecutor {
-    virtual ~IExecutor() = default;
-    template <typename Func, typename... Args>
-    // IMPORTANT NOTE: if task is periodical and you call future::get() exception will be thrown.
-    auto submit(TaskOptions opt, Func&& func, Args&&... args) -> std::future<typename std::invoke_result<Func, Args...>::type> {
-        using ReturnType = typename std::invoke_result<Func, Args...>::type;
-        if (opt.is_periodic) {
-            bool postFlag = post(std::move(std::bind(std::forward<Func>(func), std::forward<Args>(args)...)), opt);
-            return std::future<ReturnType>{};
-        }
-        auto boundPackagedTaskPtr = std::make_shared<std::packaged_task<ReturnType()>>(std::bind(std::forward<Func>(func), std::forward<Args>(args)...));
-        auto returnFuture = boundPackagedTaskPtr->get_future();
-        if (!post(std::move([boundPackagedTaskPtr]() mutable { (*boundPackagedTaskPtr)(); }), opt)) {
-            // failed to put task into executor
-            std::promise<ReturnType> p;
-            p.set_exception(std::make_exception_ptr(std::runtime_error("submit rejected by executor")));
-            return p.get_future();
-        }
-        return returnFuture;
+  virtual ~IExecutor() = default;
+  template <typename Func, typename... Args>
+  // IMPORTANT NOTE: if task is periodical and you call future::get() exception
+  // will be thrown.
+  auto submit(TaskOptions opt, Func &&func, Args &&...args)
+      -> std::future<typename std::invoke_result<Func, Args...>::type> {
+    using ReturnType = typename std::invoke_result<Func, Args...>::type;
+    if (opt.is_periodic) {
+      bool postFlag = post(std::move(std::bind(std::forward<Func>(func),
+                                               std::forward<Args>(args)...)),
+                           opt);
+      return std::future<ReturnType>{};
     }
+    auto boundPackagedTaskPtr =
+        std::make_shared<std::packaged_task<ReturnType()>>(
+            std::bind(std::forward<Func>(func), std::forward<Args>(args)...));
+    auto returnFuture = boundPackagedTaskPtr->get_future();
+    if (!post(std::move([boundPackagedTaskPtr]() mutable {
+                (*boundPackagedTaskPtr)();
+              }),
+              opt)) {
+      // failed to put task into executor
+      std::promise<ReturnType> p;
+      p.set_exception(std::make_exception_ptr(
+          std::runtime_error("submit rejected by executor")));
+      return p.get_future();
+    }
+    return returnFuture;
+  }
+
 protected:
-    virtual bool post(Task&& task, TaskOptions) = 0;
+  virtual bool post(Task &&task, TaskOptions) = 0;
 };
 
 }; // namespace stp
--- stp/RtlThreadPool.hpp
+++ stp/RtlThreadPool.hpp
@@ -2,16 +2,18 @@
 
 #include <atomic>
 #include <condition_variable>
+#include <cstdint>
+#include <exception>
 #include <functional>
 #include <future>
 #include <iostream>
 #include <memory>
 #include <mutex>
+#include <stdexcept>
 #include <thread>
 #include <vector>
 
-
-#include "UnboundedMPMCQueue.h"
+#include "UnbMpMcTemplateQueue.hpp"
 
 // TODO: add periodic task, shutdown mechanism, dynamic thread adding/removing
 namespace rtl {
@@ -21,145 +23,202 @@ using Task = std::function<void()>;
 
 class ThreadPool {
 public:
-    ThreadPool(size_t current_threads = 6, size_t max_threads = 20, size_t max_periodic_threads = 10)
-        : m_stopRequested(false)
-        , m_activeWorkers(current_threads)
-        , m_currentThreadCount(current_threads)
-        , m_maxThreadCount(max_threads)
-        , m_currentPeriodicThreadCount(0)
-        , m_maxPeriodicThreadCount(max_periodic_threads) {
-        m_currentThreadCount = std::max((size_t)1, m_currentThreadCount);
-        m_maxThreadCount = std::max(m_maxThreadCount, m_currentThreadCount);
-        m_maxPeriodicThreadCount = std::max((size_t)1, m_maxPeriodicThreadCount);
-        try {
-            m_activeWorkers.reserve(m_maxThreadCount);
-            m_periodicWorkers.reserve(m_maxPeriodicThreadCount);
-            for (size_t i = 0; i < m_currentThreadCount; ++i) {
-                add_thread();
-            }
-        } catch (const std::exception& exp) {
-            std::cerr << exp.what() << '\n';
-            throw;
-        } catch (...) {
-            std::cerr << "Unknown exception in ThreadPool constructor\n";
-            throw;
-        }
+  ThreadPool(size_t current_threads = 6, size_t max_threads = 20,
+             size_t max_periodic_threads = 10)
+      : m_currentThreadCount(current_threads), m_maxThreadCount(max_threads),
+        m_currentPeriodicThreadCount(0),
+        m_maxPeriodicThreadCount(max_periodic_threads) {
+    m_currentThreadCount = std::max((size_t)1, m_currentThreadCount.load());
+    m_maxThreadCount = std::max(m_maxThreadCount, m_currentThreadCount.load());
+    m_maxPeriodicThreadCount = std::max((size_t)1, m_maxPeriodicThreadCount);
+    try {
+      m_activeWorkers.reserve(m_maxThreadCount);
+      m_periodicWorkers.reserve(m_maxPeriodicThreadCount);
+      for (size_t i = 0; i < m_currentThreadCount; ++i) {
+        add_thread();
+      }
+    } catch (const std::exception &exp) {
+      std::cerr << exp.what() << '\n';
+      throw;
+    } catch (...) {
+      std::cerr << "Unknown exception in ThreadPool constructor\n";
+      throw;
     }
-    
-    ~ThreadPool() {
-        std::cout << "ThreadPool destructor called\n";
-        request_stop();
-        blocking_thread_stopping();
-    }
+  };
+
+  ~ThreadPool() {
+    std::cout << "ThreadPool destructor called\n";
+    request_stop();
+    blocking_thread_stopping();
+  };
+
 public:
-    template <typename F, typename... Args>
-    auto put(F&& func, Args&&... args) -> std::future<typename std::invoke_result<F, Args...>::type> {
-        if (m_stopRequested.load(std::memory_order_relaxed)) {
-            throw std::runtime_error("ThreadPool has been stopped, cannot add new tasks.");
-        }
-        using ReturnType = typename std::invoke_result<F, Args...>::type; // this return type of the function
-        auto tasking = std::make_shared<std::packaged_task<ReturnType()>>(std::bind(std::forward<F>(func), std::forward<Args>(args)...));
-        auto returnFuture = tasking->get_future();
-        try {
-            auto closureTask = [tasking]() mutable { (*tasking)(); };
-            m_taskQueue.put(std::move(closureTask));
-            return returnFuture;
-        } catch (const std::exception& exp) {
-            std::cerr << exp.what() << '\n';
-            // returnFuture.set_exception(std::current_exception());
-        } catch (...) {
-            std::cerr << "Unknown exception in ThreadPool::put\n";
-        }
-        return returnFuture;
+  template <typename F, typename... Args>
+  auto put(F &&func, Args &&...args)
+      -> std::future<typename std::invoke_result<F, Args...>::type> {
+
+    if (m_poolState.load() != PoolState::running) {
+      throw std::runtime_error{"TP is stopping or stopped"};
+    };
+    using ReturnType =
+        typename std::invoke_result<F, Args...>::type; // this return type of
+                                                       // the function
+    auto tasking = std::make_shared<std::packaged_task<ReturnType()>>(
+        std::bind(std::forward<F>(func), std::forward<Args>(args)...));
+    auto returnFuture = tasking->get_future();
+    try {
+      auto closureTask = [tasking]() mutable { (*tasking)(); };
+      bool put_result = m_taskQueue.put(std::move(closureTask));
+      if (!put_result) {
+        throw std::runtime_error{"Queue is closed"};
+      }
+      return returnFuture;
+    } catch (const std::exception &exp) {
+      std::cerr << exp.what() << '\n';
+      // returnFuture.set_exception(std::current_exception());
+      throw;
+    } catch (...) {
+      std::cerr << "Unknown exception in ThreadPool::put\n";
+      throw;
     }
 
-    template <typename F, typename... Args>
-    void put_periodic(size_t repeat_time_ms, F&& func, Args&&... args) {
-        if (m_currentPeriodicThreadCount >= m_maxPeriodicThreadCount) {
-            std::cerr << "Max periodic thread count reached\n";
-            return;
-        }
-        try {
-            add_periodic_thread(repeat_time_ms, std::bind(std::forward<F>(func), std::forward<Args>(args)...)); // copy of task?
-        } catch (const std::exception& exp) {
-            std::cerr << exp.what() << '\n';
-        } catch (...) {
-            std::cerr << "Unknown exception in ThreadPool::put_periodic\n";
-        }
-    }
+    return returnFuture;
+  };
 
-    void request_stop() {
-        m_taskQueue.request_stop();                             // sends stop signal to the queue, will return nullopt on tryTake
-        m_stopRequested.store(true, std::memory_order_relaxed); // signal threads to stop
-    }
+  template <typename F, typename... Args>
+  bool put_periodic(size_t repeat_time_ms, F &&func, Args &&...args) {
+    {
+      std::unique_lock<std::mutex> lock(m_mtx);
+      if (m_poolState.load() != PoolState::running) {
+        //   throw std::runtime_error{"TP is stopping or stopped"};
+        return false;
+      };
 
-    size_t getCurrentThreadCount() const {
-        return m_currentThreadCount;
+      try {
+        return add_periodic_thread(
+            repeat_time_ms,
+            std::bind(std::forward<F>(func),
+                      std::forward<Args>(args)...)); // copy of task?
+      } catch (const std::exception &exp) {
+        std::cerr << exp.what() << '\n';
+        return false;
+      } catch (...) {
+        std::cerr << "Unknown exception in ThreadPool::put_periodic\n";
+        return false;
+      }
     }
+    return false;
+  };
+
+  void request_stop() {
+    std::unique_lock<std::mutex> lock{m_mtx};
+    m_poolState.store(PoolState::stopping);
+    m_cv.notify_all();
+    m_taskQueue.close(); // sends stop signal to the queue, queue will not new
+    // task and continue work unless its empty
+  };
+
+  size_t getCurrentThreadCount() const { return m_currentThreadCount; }
+
 private:
-    void blocking_thread_stopping() {
-        for (size_t i = 0; i < m_periodicWorkers.size(); ++i) {
-            if (m_periodicWorkers[i].joinable()) {
-                m_periodicWorkers[i].join(); // or detach, depending on desired behavior
-            }
-        }
-        for (size_t i = 0; i < m_activeWorkers.size(); ++i) {
-            if (m_activeWorkers[i].joinable()) {
-                m_activeWorkers[i].join(); // or join, depending on desired behavior
-            }
-        }
-        // m_currentThreadCount = 0;
+  void blocking_thread_stopping() {
+    auto caller_thread_id = std::this_thread::get_id();
+    for (size_t i = 0; i < m_periodicWorkers.size(); ++i) {
+      if (m_periodicWorkers[i].get_id() == caller_thread_id) {
+        std::terminate();
+      }
+      if (m_periodicWorkers[i].joinable()) {
+        m_periodicWorkers[i].join(); // or detach, depending on desired behavior
+      }
     }
-    void add_thread() {
-        // TODO: add thread safely and checking vectors size and capacity
-        m_activeWorkers.emplace_back([this]() {
-            while (true) {
-                if (m_stopRequested.load(std::memory_order_relaxed)) {
-                    break;
-                }
-                auto task = m_taskQueue.tryTake();
-                if (!task.has_value()) {
-                    continue;
-                }
-                if (!task.value()) {
-                    continue;
-                }
-                task.value()();
+    for (size_t i = 0; i < m_activeWorkers.size(); ++i) {
+      if (m_activeWorkers[i].get_id() == caller_thread_id) {
+        std::terminate();
+      }
+      if (m_activeWorkers[i].joinable()) {
+        m_activeWorkers[i].join(); // or join, depending on desired behavior
+      }
+    }
+    m_poolState.store(PoolState::stopped);
+    // m_currentThreadCount = 0;
+  };
+
+  void add_thread() {
+    // TODO: add thread safely and checking vectors size and capacity
+    m_activeWorkers.emplace_back([this]() {
+      while (true) {
+        auto task = m_taskQueue.tryTake();
+        if (task.erc == TakeResultErrorCode::closed) {
+          return;
+        }
+        if (task.erc == TakeResultErrorCode::timeout) {
+          continue;
+        }
+
+        if (!task.result.has_value()) {
+          continue;
+        }
+        if (!task.result.value()) {
+          continue;
+        }
+        task.result.value()();
+      }
+    });
+  };
+
+  bool add_periodic_thread(size_t task_interval_ms, Task &&task) {
+
+    if (m_currentPeriodicThreadCount >= m_maxPeriodicThreadCount) {
+      std::cerr << "Max periodic thread count reached\n";
+      return false;
+    }
+    if (!task) {
+      std::cerr << "Empty task\n";
+      return false;
+    }
+    if (task_interval_ms == 0) {
+      std::cerr << "Zero interval\n";
+      return false;
+    }
+
+    // std::move? task
+    m_periodicWorkers.emplace_back(
+        [this, t = std::move(task), task_interval_ms]() {
+          while (true) {
+            if (m_poolState.load() != PoolState::running) {
+              return;
             }
+            std::unique_lock<std::mutex> lock{m_mtx};
+            m_cv.wait_for(lock, std::chrono::milliseconds(task_interval_ms),
+                          [this]() -> bool {
+                            return m_poolState.load() == PoolState::stopping;
+                          });
+            lock.unlock();
+            if (m_poolState.load() != PoolState::running) {
+              return;
+            }
+            t();
+          }
         });
-    }
-    void add_periodic_thread(size_t task_interval_ms, Task&& task) {
-        if (!task) {
-            std::cerr << "Empty task\n";
-            return;
-        }
-        if (task_interval_ms == 0) {
-            std::cerr << "Zero interval\n";
-            return;
-        }
-        ++m_currentPeriodicThreadCount;
-        // std::move? task
-        m_periodicWorkers.emplace_back([this, t = std::move(task), task_interval_ms]() {
-            while (true) {
-                if (m_stopRequested.load(std::memory_order_relaxed)) {
-                    break;
-                }
-                std::this_thread::sleep_for(std::chrono::milliseconds(task_interval_ms)); // change it on cv wait later
-                t();
-            }
-        });
-    }
-    std::atomic<bool> m_stopRequested{false};
-    std::vector<std::thread> m_activeWorkers{};
-    std::vector<std::thread> m_periodicWorkers{};
-    size_t m_currentThreadCount{0};
-    size_t m_maxThreadCount{0};
-    size_t m_currentPeriodicThreadCount{0};
-    size_t m_maxPeriodicThreadCount{0};
-    UnboundedMPMCQueue<Task> m_taskQueue{}; // add container variation
+    m_currentPeriodicThreadCount.fetch_add(1);
+
+    return true;
+  };
+
+  enum class PoolState : uint8_t { running, stopping, stopped };
+
+  UnbMpMcTemplateQueue<Task> m_taskQueue{}; // add container variation
+  std::condition_variable m_cv;
+  mutable std::mutex m_mtx;
+  std::vector<std::thread> m_activeWorkers{};
+  std::vector<std::thread> m_periodicWorkers{};
+  std::atomic<PoolState> m_poolState{PoolState::running};
+  std::atomic<size_t> m_currentThreadCount{0};
+  size_t m_maxThreadCount{0};
+  std::atomic<size_t> m_currentPeriodicThreadCount{0};
+  size_t m_maxPeriodicThreadCount{0};
 };
 
 }; // namespace stp
-
 
 }; // namespace rtl
